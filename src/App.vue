7221beb23ccc1e2aa634ae6d2eeeef2e9284fe4d
Mitchell Valentine <mitchell.valentinecrumb@cffp.edu>
2018-11-14 08:25:12 -0700

    fresh start

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div#app
+    v-navigation-drawer(app)
+    v-toolbar(app)
+      router-link(to="/") Home
+      router-link(to="/about") About
+    v-content
+      v-container(fluid)
+        router-view
+    v-footer(app)
+</template>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div.home
+    h1 Mitchell Valentine
+</template>
+<script>
+
+export default {
+  name: 'home',
+  components: {
+    
+  }
+}
+</script>
